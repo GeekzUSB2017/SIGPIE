48a6ad3fe1d1b8e6a005a0a6d65244658e3f6995
Veronica <vemazutiel@gmail.com>
2017-02-24 01:43:09 -0400

    Refactoring views a css files

--- parameters_8000.py
+++ parameters_8000.py
@@ -1,1 +1,1 @@
-password="pbkdf2(1000,20,sha512)$acb53e3dcd3f03ea$7c1233e2a7ae7afb126f2b40d9efe0f61a0ca9d0"
+password="pbkdf2(1000,20,sha512)$aa336f2559cbb412$607b98cc70693dca0fde4ad1f00d1def97d5be55"
