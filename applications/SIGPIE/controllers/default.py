5913923bc7953c697ce3947e239a26adeccc0f13
Gianni Manilia <giannimanilia@gmail.com>
2017-03-09 21:59:20 -0400

    Controlador renuncia
    Controlador sencillo para que el estudiante renuncie

--- applications/SIGPIE/controllers/default.py
+++ applications/SIGPIE/controllers/default.py
@@ -20,6 +20,29 @@ def about():
 	else:
 		redirect(URL('index'))
 
+def renuncia():
+	if session.usuario is not None:
+		form = SQLFORM(db.renuncia)
+		return dict()
+	else:
+		redirect(URL('index'))
+
+	if session.usuario is not None:
+		estudiante = db(db.estudiante.carnet == session.usuario['usbid']).select().first()
+		renuncia = db(db.renuncia.estudiante == estudiante.id).select().first()
+
+		if renuncia != None:
+			record = db.renuncia(renuncia.id)
+			form = SQLFORM(db.renuncia, record, hidden=dict(estudiante=estudiante.id))
+		else:
+			form = SQLFORM(db.renuncia, hidden=dict(estudiante=estudiante.id))
+
+		if form.process().accepted:
+			redirect(URL('index'))
+
+		return dict(form_renuncia = form)
+	else:
+		redirect(URL('index'))
 
 def postularse():
 	if session.usuario is not None:
