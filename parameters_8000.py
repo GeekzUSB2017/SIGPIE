fc91072caecf71b56bf8647160250442d2dcd793
Julio Fuenmayor <jfuenmayor96@gmail.com>
2017-03-18 21:38:09 -0400

    mas pruebas con la generacion de PDFs a traves de FPDF Templates
    Se actualiza FPDF a la ultima version disponible en la pagina

--- parameters_8000.py
+++ parameters_8000.py
@@ -1,1 +1,1 @@
-password="pbkdf2(1000,20,sha512)$b731262b2ce1df76$6c948ffbbebf7d73a5b8450f1efd7ca6af9d4485"
+password="pbkdf2(1000,20,sha512)$a7c432cfc5c6cda4$50dbbc9aa019b3b28a24ec42a3d7ed93a56e1283"
