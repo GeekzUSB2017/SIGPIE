525c8fe328895a61efc669063c63baa5885fe08e
gabo <gabo2595@gmail.com>
2017-03-21 21:16:40 -0400

    Para merge Para merge

--- parameters_8000.py
+++ parameters_8000.py
@@ -1,1 +1,1 @@
-password="pbkdf2(1000,20,sha512)$9189b05de60cdf5d$42e685b70ff8a4a3aeaa97acc2003820b3988d7c"
+password="pbkdf2(1000,20,sha512)$a4a5b7fe5dfbf15f$89d8342c485aefb5446cdadf0bf15bf305432d63"
