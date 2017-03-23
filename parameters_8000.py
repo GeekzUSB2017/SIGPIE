4342019f48e242d67c5cef667c6f7a4b5203e453
melanie2201 <melanie220196@gmail.com>
2017-03-23 15:29:50 -0400

    agregado jquerypara postrgado que no salga carrera

--- parameters_8000.py
+++ parameters_8000.py
@@ -1,1 +1,1 @@
-password="pbkdf2(1000,20,sha512)$b90e774ada86b5f5$cc91a6d7b4360daa68079cb2f5f9dd5fbedf9056"
+password="pbkdf2(1000,20,sha512)$9f5dfd6530f4eb5e$59790908686e58fa209bab1566eaea3e6cb33b55"
