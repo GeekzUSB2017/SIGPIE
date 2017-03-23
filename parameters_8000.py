56b5beec81216fa2ad0d4f77cf3d34c3726b2112
Julio Fuenmayor <jfuenmayor96@gmail.com>
2017-03-23 14:46:33 -0400

    se agrega el postgrado y otro tipo de parentezco al formulario en pdf

--- parameters_8000.py
+++ parameters_8000.py
@@ -1,5 +1,1 @@
-<<<<<<< HEAD
-password="pbkdf2(1000,20,sha512)$a9cdf0e736b64f2c$125d85f4b088db1ab8c41fa038524b669f1f35a9"
-=======
-password="pbkdf2(1000,20,sha512)$b90e774ada86b5f5$cc91a6d7b4360daa68079cb2f5f9dd5fbedf9056"
->>>>>>> 9293b635d538df8852bb3ac54acee6c7385619fd
+password="pbkdf2(1000,20,sha512)$b095c72d07429f94$7ccc34acdd2d38ef4a963e9aa54451662960a8c9"
